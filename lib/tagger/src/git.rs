3e573f875c3f10f8eea421d92733576699e3bce8
icylisper <icylisper@gmail.com>
2025-01-12 06:47:24 -0800

    Restructure library components - move to lib/

---
+++ lib/tagger/src/git.rs
@@ -0,0 +1,73 @@
+use kit::{pwd, sh, triml};
+use regex::Regex;
+use semver::Version;
+
+pub fn extract_version(s: &str) -> String {
+    let re: Regex = Regex::new(r"(?:(\d+)\.)?(?:(\d+)\.)?(?:(\d+)\.\d+)").unwrap();
+    let matches = re.find(s);
+    match matches {
+        Some(m) => m.as_str().to_string(),
+        _ => "0.0.2".to_string(),
+    }
+}
+
+pub fn maybe_semver(v: &str) -> Version {
+    Version::parse(v).unwrap()
+}
+
+pub fn latest_version(prefix: &str) -> String {
+    let cmd = format!("git describe --tags --abbrev=0 --match {}-*", prefix);
+    let out = sh(&cmd, &pwd());
+    if out.contains("fatal: No names found") {
+        String::from("0.0.1")
+    } else {
+        extract_version(&out)
+    }
+}
+
+pub fn tag_revision(tag: &str) -> String {
+    let cmd = format!("git rev-parse {}", tag);
+    sh(&cmd, &pwd())
+}
+
+pub fn commit_message(tag: &str) -> String {
+    let cmd = format!("git show --pretty=format:\"%B\" -n1 --no-patch {} | grep -v Co-authored | grep -iv \"Merge pull\" | head -n1", tag);
+    triml(&sh(&cmd, &pwd())).to_string()
+}
+
+pub fn changelog(from_sha: &str, to_sha: &str) -> String {
+    let cmd = format!("git log --pretty=\"- %s\" {}...{} .", from_sha, to_sha);
+    let out = sh(&cmd, &pwd());
+    if out.contains("fatal") {
+        String::from("")
+    } else {
+        out
+    }
+}
+
+pub fn fetch_tags() {
+    sh("git fetch --tags", &pwd());
+}
+
+pub fn create_tag(tag: &str, parent: Option<String>) {
+    let cmd = match parent {
+        Some(p) => format!("git tag -f {} {}", tag, p),
+        None => format!("git tag {}", tag),
+    };
+    sh(&cmd,& pwd());
+}
+
+pub fn create_annotated_tag(tag: &str, parent: Option<String>) {
+    let cmd =  match parent {
+       Some(p) => format!("git -c user.name=tc-releaser -c user.email=tc-releaser@informed.iq tag -a {} {} -m \"{} release\"", tag, p, tag),
+       None => format!("git tag {}", tag)
+    };
+    let out = sh(&cmd, &pwd());
+    println!("annot: {}", out);
+}
+
+pub fn push_tag(tag: &str) {
+    let cmd = format!("git push origin {}", tag);
+    let out = sh(&cmd, &pwd());
+    println!("{}", out);
+}
---
+++ lib/tagger/src/lib.rs
@@ -0,0 +1,251 @@
+use notifier::RichText;
+use kit as u;
+use kit::*;
+mod git;
+
+fn inc_patch(v: &str) -> String {
+    let version = git::maybe_semver(v);
+    let mut next = version.clone();
+    next.increment_patch();
+    format!("{}.{}.{}", next.major, next.minor, next.patch)
+}
+
+fn inc_minor(v: &str) -> String {
+    let version = git::maybe_semver(v);
+    let mut next = version.clone();
+    next.increment_minor();
+    format!("{}.{}.0", next.major, next.minor)
+}
+
+fn inc_major(v: &str) -> String {
+    let mut version = git::maybe_semver(v);
+    version.increment_major();
+    format!("{}.0.0", version.major)
+}
+
+fn has_patch(v: &str) -> bool {
+    let version = git::maybe_semver(v);
+    version.patch > 0
+}
+
+fn add_suffix(v: &str, suffix: &str) -> String {
+    let version = git::maybe_semver(v);
+    let next = version.clone();
+    format!("{}.{}.{}-{}", next.major, next.minor, next.patch, suffix)
+}
+
+fn dec_minor(v: &str) -> String {
+    let version = git::maybe_semver(v);
+    let next = version.clone();
+    if next.minor > 0 {
+        let minor = next.minor - 1;
+        if minor == 0 {
+            format!("{}.0.2", next.major)
+        } else {
+            format!("{}.{}.0", next.major, minor)
+        }
+    } else {
+        format!("{}.{}.0", next.major, next.minor)
+    }
+}
+
+fn current_stable_minor(v: &str) -> String {
+    let version = git::maybe_semver(v);
+    let next = version.clone();
+    if next.minor > 0 {
+        format!("{}.{}.0", next.major, next.minor)
+    } else {
+        format!("{}.{}.0", next.major, next.minor)
+    }
+}
+
+fn changelog(from_tag: &str, to_tag: &str) -> String {
+    let from_sha = git::tag_revision(from_tag);
+    let to_sha = git::tag_revision(to_tag);
+    git::changelog(&from_sha, &to_sha)
+}
+
+pub fn changelog_since_last(prefix: &str, version: &str, has_suffix: bool) -> String {
+    let prev_ver;
+    if has_suffix {
+        prev_ver = current_stable_minor(version);
+    } else {
+        prev_ver = dec_minor(version);
+    }
+
+    println!("{}", prev_ver);
+    let curr_tag = format!("{}-{}", prefix, version);
+    let prev_tag = format!("{}-{}", prefix, prev_ver);
+    changelog(&prev_tag, &curr_tag)
+}
+
+// git
+
+fn should_tag(tag: &str) -> bool {
+    let dir = u::pwd();
+    let c1 = format!("git rev-parse {}", tag);
+    let c2 = format!("git log -n 1 --format=%H .");
+    let c1_out = u::sh(&c1, &dir);
+    let c2_out = u::sh(&c2, &dir);
+
+    if !c1_out.contains("fatal: ambiguous argument") {
+        println!("tag: {c1_out} ({tag})");
+    }
+    println!("rev: {c2_out}");
+    c1_out != c2_out
+}
+
+
+#[derive(Clone, Debug)]
+pub struct Tag {
+    pub prefix: String,
+    pub parent: String,
+    pub create: bool,
+    pub version: String,
+}
+
+pub fn next_tag(prefix: &str, next: &str, suffix: &str) -> Tag {
+    match next {
+        "major" => {
+            let git_ver = git::latest_version(prefix);
+            Tag {
+                parent: git_ver.clone(),
+                prefix: s!(prefix),
+                create: true,
+                version: inc_major(&git_ver),
+            }
+        }
+
+        "minor" => {
+            let git_ver = git::latest_version(prefix);
+            Tag {
+                parent: git_ver.clone(),
+                prefix: s!(prefix),
+                create: has_patch(&git_ver),
+                version: match suffix {
+                    "default" => inc_minor(&git_ver),
+                    _ => add_suffix(&git_ver, suffix),
+                },
+            }
+        }
+
+        "patch" => {
+            let git_ver = git::latest_version(prefix);
+            let current_tag = format!("{}-{}", prefix, &git_ver);
+            Tag {
+                parent: git_ver.clone(),
+                prefix: s!(prefix),
+                create: should_tag(&current_tag),
+                version: match suffix {
+                    "default" => inc_patch(&git_ver),
+                    _ => add_suffix(&git_ver, suffix),
+                },
+            }
+        }
+
+        _ => Tag {
+            parent: u::empty(),
+            prefix: u::empty(),
+            create: false,
+            version: u::empty(),
+        },
+    }
+}
+
+fn fmt_msg(prefix: &str, version: &str, parent: &str, changes: &str) -> String {
+    let title = &format!("QA Release | {} ", u::simple_date());
+    let summary = &format!("*{}* - `{}` (annot on: {})", prefix, version, parent);
+    let rt = RichText::new(title, summary, &changes);
+    serde_json::to_string(&rt).unwrap()
+}
+
+async fn dry_run(next: &str, tag: Tag, has_suffix: bool) {
+    let Tag {
+        parent,
+        prefix,
+        version,
+        ..
+    } = tag;
+    git::fetch_tags();
+
+    match next {
+        "patch" => {
+            let tag = format!("{}-{}", &prefix, &version);
+            let commit_msg = git::commit_message(&tag);
+            println!("{}", commit_msg);
+        }
+        "minor" => {
+            let changes = changelog_since_last(&prefix, &version, has_suffix);
+            let msg = fmt_msg(&prefix, &version, &parent, &changes);
+            notifier::slack(&prefix, msg).await;
+        }
+        _ => println!("Nothing to do.."),
+    }
+}
+
+async fn create(next: &str, tag: Tag, push: bool, has_suffix: bool) {
+    let Tag {
+        parent,
+        prefix,
+        version,
+        create,
+        ..
+    } = tag;
+
+    match next {
+        "patch" => {
+            let tag = format!("{}-{}", &prefix, &version);
+            if create {
+                println!("Creating tag {}", &tag);
+                git::create_tag(&tag, None);
+                if push {
+                    git::push_tag(&tag);
+                    let commit_msg = git::commit_message(&tag);
+                    let msg = format!("Created Patch Release {} -{}", tag, commit_msg);
+                    notifier::slack(&prefix, notifier::wrap_msg(&msg)).await;
+                }
+            } else {
+                println!("Not tagging or releasing {}", &tag);
+            }
+        }
+
+        "minor" => {
+            let tag = format!("{}-{}", prefix, version);
+
+            if create {
+                git::fetch_tags();
+                let parent_tag = format!("{}-{}", prefix, parent);
+
+                println!("Creating minor git tag {}", &tag);
+                let parent_revision = git::tag_revision(&parent_tag);
+
+                println!("{} {}", &tag, &parent_revision);
+                git::create_annotated_tag(&tag, Some(parent_revision.clone()));
+
+                if push {
+                    git::push_tag(&tag);
+                    let changes = changelog_since_last(&prefix, &version, has_suffix);
+                    let msg = fmt_msg(&prefix, &version, &parent, &changes);
+                    println!("{}", &msg);
+                    notifier::slack(&prefix, msg.clone()).await;
+                    notifier::slack("QA", msg).await;
+                }
+            } else {
+                println!("Not creating {}", &tag);
+            }
+        }
+
+        _ => println!("Nothing to do yet"),
+    }
+}
+
+pub async fn create_tag(next: &str, prefix: &str, suffix: &str, push: bool, is_dry_run: bool) {
+    let tag = next_tag(&prefix, &next, &suffix);
+    let has_suffix = suffix != "default";
+    if is_dry_run {
+        println!("dry: {:?}", tag);
+        dry_run(&next, tag, has_suffix).await;
+    } else {
+        create(&next, tag, push, has_suffix).await;
+    }
+}
