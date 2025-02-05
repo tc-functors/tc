1a4c74aa9f4208df9adc819b5ec22ab8dc63834c
icylisper <icylisper@gmail.com>
2025-02-05 14:50:02 -0800

    Remove stale print in builder

--- lib/builder/src/python/code.rs
+++ lib/builder/src/python/code.rs
@@ -1,7 +1,10 @@
 use kit::sh;
 
-pub fn build(dir: &str, command: &str) -> String {
+pub fn build(dir: &str, command: &str, trace: bool) -> String {
     let c = format!(r"{}", command);
+    if trace {
+        println!("{}", &c);
+    }
     sh(&c, dir);
     format!("{}/lambda.zip", dir)
 }
