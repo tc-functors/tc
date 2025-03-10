24bf1a4d14e3af91fbf7c8347a563568f2491b6c
icylisper <icylisper@gmail.com>
2025-03-09 21:04:05 -0700

    Fixes rust build - use cargo lambda

--- lib/builder/src/rust/inline.rs
+++ lib/builder/src/rust/inline.rs
@@ -1,9 +1,38 @@
-use colored::Colorize;
 use kit as u;
+
+fn gen_dockerfile(dir: &str) {
+    let f = format!(
+        r#"
+FROM ghcr.io/cargo-lambda/cargo-lambda:latest
+
+WORKDIR /build
+COPY . .
+
+RUN cargo lambda build --release
+"#
+    );
+    let dockerfile = format!("{}/Dockerfile", dir);
+    u::write_str(&dockerfile, &f);
+}
+
+
+fn copy_from_docker(dir: &str) {
+    let temp_cont = &format!("tmp-{}", u::basedir(dir));
+    let clean = &format!("docker rm -f {}", &temp_cont);
+
+    let run = format!("docker run -d --name {} {}", &temp_cont, u::basedir(dir));
+    u::sh(&clean, dir);
+    u::sh(&run, dir);
+    let id = u::sh(&format!("docker ps -aqf \"name={}\"", temp_cont), dir);
+    tracing::debug!("Container id: {}", &id);
+
+    u::sh(&format!("docker cp {}:/build/target/lambda/bootstrap/bootstrap bootstrap", id), dir);
+    u::sh(&clean, dir);
+    u::sh("rm -f Dockerfile wrapper", dir);
+}
 
 pub fn build(dir: &str) -> String {
 
-    println!("Building {} (rust)", u::basedir(dir).blue());
     let no_docker = match std::env::var("TC_NO_DOCKER_BUILD") {
         Ok(_) => true,
         Err(_) => false
@@ -16,20 +45,19 @@ pub fn build(dir: &str) -> String {
         ];
         u::run_seq(cmds, dir);
     } else {
-        let cmd = vec![
-            "docker run --rm",
-            "-v `pwd`:/code -w /code",
-            "-v ${HOME}/.cargo/registry:/cargo/registry",
-            "-v ${HOME}/.cargo/git:/cargo/git",
-            "-u $(id -u):$(id -g)",
-            "rustserverless/lambda-rust",
-        ];
-        u::runv(dir, cmd);
-        u::runcmd_stream("cp target/*/release/bootstrap bootstrap", dir);
-        u::runcmd_stream("rm -rf build target", dir);
+        gen_dockerfile(dir);
+        let name = u::basedir(dir);
+        u::runcmd_stream(&format!("docker build --no-cache  -t {} .", name), dir);
+        copy_from_docker(dir);
+        u::runcmd_stream("rm -rf build target Dockerfile", dir);
     }
+    if !u::path_exists(dir, "bootstrap") {
+        panic!("Building failed");
+    }
+
     let size = u::path_size(dir, "bootstrap");
-    println!("Built bootstrap ({})", u::file_size_human(size));
+
+   println!("Built bootstrap ({})", u::file_size_human(size));
     let command = "zip -q -r lambda.zip bootstrap";
     u::sh(command, dir);
     format!("{}/lambda.zip", dir)
