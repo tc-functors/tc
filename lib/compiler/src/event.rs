30ecfeb99d64e70625aa647c13f9dcdbafff54a7
icylisper <icylisper@gmail.com>
2025-03-31 08:45:36 -0700

    wip - inspector routes refactor

--- lib/compiler/src/event.rs
+++ lib/compiler/src/event.rs
@@ -1,6 +1,7 @@
 use kit::*;
 use serde_derive::{Deserialize, Serialize};
 use std::collections::HashMap;
+use crate::spec::Consumes;
 use super::template;
 use configurator::Config;
 
@@ -22,11 +23,21 @@ impl TargetKind {
     }
 }
 
+
+fn as_ns(given: &Option<String>, s: &str) -> String {
+    match given {
+        Some(p) => s!(p),
+        None => kit::split_first(s, "/")
+    }
+}
+
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Target {
     pub kind: TargetKind,
     pub id: String,
     pub name: String,
+    pub producer_ns: String,
+    pub consumer_ns: String,
     pub arn: String,
     pub role_arn: String,
     pub input_paths_map: Option<HashMap<String, String>>,
@@ -40,6 +51,8 @@ impl Target {
         id: &str,
         name: &str,
         arn: &str,
+        producer_ns: &str,
+        consumer_ns: &str,
         input_paths_map: Option<HashMap<String, String>>,
         input_template: Option<String>,
 
@@ -50,6 +63,8 @@ impl Target {
             kind: kind,
             id: s!(id),
             name: s!(name),
+            producer_ns: s!(producer_ns),
+            consumer_ns: s!(consumer_ns),
             arn: s!(arn),
             role_arn: template::role_arn("tc-base-event-role"),
             input_paths_map: input_paths_map,
@@ -59,20 +74,40 @@ impl Target {
 }
 
 pub fn make_targets(
+    namespace: &str,
     event_name: &str,
-    function: Option<String>,
-    mutation: Option<String>,
-    stepfunction: Option<String>,
-    fallback_fqn: &str,
-    functions: Vec<String>
+    consumes: &Consumes,
+    fallback_fqn: &str
 ) -> Vec<Target> {
 
+    let Consumes {
+        producer_ns,
+        producer,
+        function,
+        mutation,
+        functions,
+        stepfunction,
+        ..
+    } = consumes;
 
     let mut xs: Vec<Target> = vec![];
+
+    let producer_ns = as_ns(producer_ns, producer);
+    let consumer_ns = namespace;
+
     if let Some(ref f) = function {
         let id = format!("{}_lambda_target", event_name);
         let arn = template::lambda_arn(&f);
-        let t = Target::new(TargetKind::Function, &id, &f, &arn, None, None);
+        let t = Target::new(
+            TargetKind::Function,
+            &id,
+            &f,
+            &arn,
+            &producer_ns,
+            &consumer_ns,
+            None,
+            None
+        );
         xs.push(t);
     }
 
@@ -80,7 +115,16 @@ pub fn make_targets(
         for f in functions {
             let id = format!("{}_{}_target", event_name, &f);
             let arn = template::lambda_arn(&f);
-            let t = Target::new(TargetKind::Function, &id, &f, &arn, None, None);
+            let t = Target::new(
+                TargetKind::Function,
+                &id,
+                &f,
+                &arn,
+                &producer_ns,
+                &consumer_ns,
+                None,
+                None
+            );
             xs.push(t);
         }
     }
@@ -93,15 +137,32 @@ pub fn make_targets(
         let input_paths_map = Some(h);
         let input_template = Some(format!(r##"{{"detail": <detail>}}"##));
 
-        // FIXME:
         let arn = "unresolved";
-        let t = Target::new(TargetKind::Mutation, &id, m, &arn, input_paths_map, input_template);
+        let t = Target::new(
+            TargetKind::Mutation,
+            &id,
+            m,
+            &arn,
+            &producer_ns,
+            &consumer_ns,
+            input_paths_map,
+            input_template
+        );
         xs.push(t);
     }
     if let Some(ref s) = stepfunction {
         let id = format!("{}_target", event_name);
         let arn = template::sfn_arn(s);
-        let t = Target::new(TargetKind::StepFunction, &id, s, &arn,  None, None);
+        let t = Target::new(
+            TargetKind::StepFunction,
+            &id,
+            s,
+            &arn,
+            &producer_ns,
+            &consumer_ns,
+            None,
+            None
+        );
         xs.push(t)
     }
 
@@ -109,7 +170,16 @@ pub fn make_targets(
     if mutation.is_none() && function.is_none() && stepfunction.is_none() {
         let id = format!("{}_target", event_name);
         let arn = template::sfn_arn(fallback_fqn);
-        let t = Target::new(TargetKind::StepFunction, &id, fallback_fqn, &arn, None, None);
+        let t = Target::new(
+            TargetKind::StepFunction,
+            &id,
+            fallback_fqn,
+            &arn,
+            &producer_ns,
+            &consumer_ns,
+            None,
+            None
+        );
         xs.push(t)
     }
     xs
@@ -145,7 +215,6 @@ pub struct EventPattern {
     pub detail: Option<Detail>,
 }
 
-
 impl EventPattern {
 
     fn new(event_name: &str, source: &str, filter: Option<String>) -> EventPattern {
@@ -167,46 +236,53 @@ pub struct Event {
     pub bus_arn: String,
     pub pattern: EventPattern,
     pub targets: Vec<Target>,
-    pub sandboxes: Vec<String>
+    pub sandboxes: Vec<String>,
 }
 
 impl Event {
 
     pub fn new(
-        name: &str,
-        rule_name: Option<String>,
-        producer: &str,
-        filter: Option<String>,
-        pattern: Option<String>,
+        event_name: &str,
+        consumer_spec: &Consumes,
         targets: Vec<Target>,
-        sandboxes: Vec<String>,
         config: &Config
-
     ) -> Event {
+
+        let Consumes {
+            rule_name,
+            producer,
+            filter,
+            pattern,
+            sandboxes,
+            ..
+        } = consumer_spec;
+
 
         let pattern = match pattern {
             Some(p) => {
                 let pp: EventPattern = serde_json::from_str(&p).unwrap();
                 pp
             },
-            None => EventPattern::new(name, producer, filter)
+            None => EventPattern::new(event_name, producer, filter.clone())
         };
 
         let bus = &config.aws.eventbridge.bus;
         let rule_prefix = &config.aws.eventbridge.rule_prefix;
         let rule_name = match rule_name {
-            Some(r) => r,
-            None => format!("{}{{{{namespace}}}}-{}-{{{{sandbox}}}}", rule_prefix, s!(name))
+            Some(r) => s!(r),
+            None => format!("{}{{{{namespace}}}}-{}-{{{{sandbox}}}}",
+                            rule_prefix, s!(event_name))
         };
 
+
         Event {
-            name: s!(name),
+            name: s!(event_name),
             rule_name: rule_name,
             bus: bus.clone(),
             bus_arn: template::event_bus_arn(bus),
             pattern: pattern,
             targets: targets,
-            sandboxes: sandboxes
+            sandboxes: sandboxes.clone(),
         }
     }
 }
