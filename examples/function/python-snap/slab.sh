dde173eaa2c023cbc82bb75df2f64921482028b3
icylisper <icylisper@gmail.com>
2025-04-05 15:12:34 -0700

    Adds slab example

---
+++ examples/function/python-snap/slab.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e -u -o pipefail
+
+pip download -vv --platform manylinux2014_x86_64 --only-binary=:all: --dest /build/python \
+    aws-lambda-powertools \
+    aws-xray-sdk \
+    honeybadger \
+    opencv-python-headless \
+    matplotlib \
+    transformers \
+    pillow \
+    wrapt \
+    tabulate \
+    nltk \
+    urllib3 \
+    pandas \
+    xgboost \
+    gensim \
+    scikit-learn \
+    spacy \
+    backoff \
+    psycopg2-binary \
+    ultralytics \
+    fasttext-wheel \
+    torch==2.3.0 \
+    torchvision==0.18.0
