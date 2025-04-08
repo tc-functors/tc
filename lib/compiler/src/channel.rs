102c3477bb6b57d5b8330073548ea77b5a0d43d3
icylisper <icylisper@gmail.com>
2025-04-08 15:21:32 -0700

    Adds missing modules - channel

---
+++ lib/compiler/src/channel.rs
@@ -0,0 +1,56 @@
+use std::collections::HashMap;
+use serde_derive::{Deserialize, Serialize};
+use super::template;
+use super::spec::{ChannelSpec, HandlerSpec};
+
+use kit as u;
+
+fn default_handler() -> String {
+    format!(r#"export function onSubscribe(ctx) {{
+       return ctx.events
+}}
+
+export function onPublish(ctx) {{
+  return ctx.events }}
+"#)
+}
+
+fn event_handler(event_name: &str) -> String {
+    default_handler()
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct Channel {
+    pub handler: String,
+    pub name: String,
+    pub api_name: String
+}
+
+fn find_handler(hs: &HandlerSpec) -> String {
+    let HandlerSpec { handler, event, .. } = hs;
+    if let Some(h) = handler {
+        match h.as_ref() {
+            "default" => default_handler(),
+            _  => u::slurp(&h)
+        }
+    } else {
+        match event {
+            Some(e) => event_handler(&e),
+            None => default_handler()
+        }
+    }
+}
+
+pub fn make(namespace: &str, spec: HashMap<String, ChannelSpec>) -> HashMap<String, Channel> {
+    let mut h: HashMap<String, Channel> = HashMap::new();
+    for (name, s) in spec {
+        let handler = find_handler(&s.on_publish);
+         let c = Channel {
+            name: name.clone(),
+            handler: handler,
+            api_name: template::topology_fqn(namespace, false)
+        };
+        h.insert(name, c);
+    }
+    h
+}
---
+++ lib/deployer/src/channel.rs
@@ -0,0 +1,18 @@
+use compiler::Channel;
+use aws::appsync;
+use aws::Env;
+use std::collections::HashMap;
+
+pub async fn create(env: &Env, channels: &HashMap<String, Channel>) {
+    let client = appsync::make_client(&env).await;
+
+    for (_, channel) in channels {
+        println!("Creating channel: {}", &channel.name);
+        let api_id = appsync::create_events_api(&client, &channel.api_name).await;
+        appsync::create_events_channel(&client, &api_id, &channel.name, &channel.handler).await;
+    }
+}
+
+pub async fn delete(env: &Env, channels: &HashMap<String, Channel>) {
+
+}
