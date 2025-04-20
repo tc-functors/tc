2d88e9dc1e5bc5f4207b25003610a202ded228da
icylisper <icylisper@gmail.com>
2025-04-20 10:58:04 -0700

    Check login status before publish

--- lib/publisher/src/ecr.rs
+++ lib/publisher/src/ecr.rs
@@ -2,17 +2,50 @@ use aws::Env;
 use aws::ecr;
 
 use std::collections::HashMap;
+use serde_derive::{Deserialize};
+use base64::{engine::general_purpose, Engine as _};
 use kit as u;
 
 fn get_host(env: &Env) -> String {
     format!("{}.dkr.ecr.{}.amazonaws.com", env.account(), env.region())
 }
 
-fn is_logged_in() -> bool {
-    // let data = u::slurp(&u::expand_path("~/.docker/config"));
-    // let res: serde_json::Value = serde_json::from_str(&data).expect("Unable to parse");
-    // println!("{}", &res["auths"]);
-    true
+#[derive(Deserialize, Clone, Debug)]
+struct Auth {
+    auth: String
+}
+
+type Config = HashMap<String, HashMap<String, Auth>>;
+
+fn is_logged_in(env: &Env) -> bool {
+    let cfg_file = &u::expand_path("~/.docker/config.json");
+    if !u::file_exists(cfg_file) {
+        return false
+    }
+    let data = u::slurp(cfg_file);
+    let config: Config = serde_json::from_str(&data).expect("Unable to parse");
+    let host = get_host(env);
+    let maybe_auth = match config.get("auths") {
+        Some(x) => match x.get(&host) {
+            Some(g) => Some(g.auth.clone()),
+            None => None
+        },
+        None => None
+    };
+
+    match maybe_auth {
+        Some(auth) => {
+            let bytes1 = general_purpose::STANDARD.decode(auth).unwrap();
+            let sa1 = &String::from_utf8_lossy(&bytes1)[4..];
+            let bytes2 = general_purpose::STANDARD.decode(sa1).unwrap();
+            let sa = String::from_utf8_lossy(&bytes2);
+            let val: serde_json::Value = serde_json::from_str(&sa).unwrap();
+            let exp = &val["expiration"].as_i64().unwrap();
+            let now = u::current_millis() / 1000;
+            (exp - now) > 60
+        },
+        None => false
+    }
 }
 
 pub async fn login(env: &Env, dir: &str) {
@@ -25,7 +58,7 @@ pub async fn login(env: &Env, dir: &str) {
 
 pub async fn publish(env: &Env, image_name: &str) {
     let dir = kit::pwd();
-    if !is_logged_in() {
+    if !is_logged_in(env) {
         login(env, &dir).await;
     }
     let cmd = format!("docker push {}", image_name);
