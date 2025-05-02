c21bf8033f76ae96c0f1267c24bbaece65ec8fba
icylisper <icylisper@gmail.com>
2025-05-02 10:37:37 -0700

    Adds venv to dockerignore

--- examples/topologies/01-functions-build/python-snap/handler.py
+++ examples/topologies/01-functions-build/python-snap/handler.py
@@ -1,7 +1,7 @@
 from importer import LambdaImporter
 
 def handler(event, context):
-  imp = LambdaImporter(bucket_name="iq-artifacts-cicd-uswest2")
+  imp = LambdaImporter(bucket_name=BUCKET)
   a = imp.import_package("torch_zip", "libtorch.zip")
   print(a)
   b = imp.import_package("torch", "torch.whl")
