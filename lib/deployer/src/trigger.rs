3d2d9bfd90186f34a8015f5fc8854ee144e5621b
icylisper <icylisper@gmail.com>
2025-05-15 09:58:36 -0700

    Adds pools to topology

--- lib/deployer/src/trigger.rs
+++ lib/deployer/src/trigger.rs
@@ -1,27 +1,18 @@
 use authorizer::Auth;
 use crate::aws::cognito;
 use std::collections::HashMap;
-use kit as u;
 
-fn abbr(name: &str) -> String {
-    if name.chars().count() > 15 {
-        u::abbreviate(name, "-")
-    } else {
-        name.to_string()
-    }
+pub async fn delete(_auth: &Auth, _pools: Vec<String>) {
+    // let client = cognito::make_client(auth).await;
+    // for pool in pools {
+    //     //cognito::delete_pool(&client, &pool).await
+    // }
 }
 
-pub async fn delete(auth: &Auth, fqn: &str) {
-    let fqn = abbr(fqn);
+pub async fn create(auth: &Auth, pools: Vec<String>, triggers: HashMap<String, String>) {
     let client = cognito::make_client(auth).await;
-    println!("Deleting triggers (cognito) {}", &fqn);
-    cognito::delete_pool(&client, &fqn).await
-}
-
-pub async fn create(auth: &Auth, fqn: &str, triggers: HashMap<String, String>) {
-    let client = cognito::make_client(auth).await;
-    let fqn = abbr(fqn);
     let mappings = cognito::make_lambda_mappings(triggers);
-    println!("Creating triggers (cognito) {}", &fqn);
-    cognito::create_or_update_pool(&client, &fqn, mappings).await
+    for pool in pools {
+        cognito::create_or_update_pool(&client, &pool, mappings.clone()).await
+    }
 }
