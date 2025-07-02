da68cd642d4e65cd42106d1b7f037b355c3c8475
icylisper <icylisper@gmail.com>
2025-07-02 12:28:56 -0700

    Restructure examples

---
+++ examples/pages/cljs/src/play/core.cljs
@@ -0,0 +1,3 @@
+(ns play.core)
+
+(println "First cljs")
---
+++ examples/pages/spa/src/App.svelte
@@ -0,0 +1,50 @@
+<script>
+  import { Router, Link, Route } from "svelte-routing";
+
+  import Home from './Home.svelte'
+  import Foo from './Foo.svelte'
+  import Bar from './Bar.svelte'
+  import Baz from './Baz.svelte'
+</script>
+
+<main>
+
+  <Router>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/foo">Foo</Link>
+      <Link to="/bar">Bar</Link>
+      <Link to="/baz">Baz</Link>
+    </nav>
+
+    <div class="box">
+      <Route path="/" component={Home}/>
+      <Route path="/foo" component={Foo}/>
+      <Route path="/bar" component={Bar}/>
+      <Route path="/baz" component={Baz}/>
+
+    </div>
+  </Router>
+
+</main>
+
+
+<style>
+
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  .box {
+    padding-top: 25px;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
