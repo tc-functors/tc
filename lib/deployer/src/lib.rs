302b4ca0de00c20e3de0355082d56aac82823b8f
icylisper <icylisper@gmail.com>
2025-08-07 07:09:17 -0700

    Restructure releaser module
    - releaser is deprecated

--- lib/deployer/src/lib.rs
+++ lib/deployer/src/lib.rs
@@ -374,3 +374,24 @@ pub async fn try_list(auth: &Auth, topology: &Topology, maybe_entity: &Option<St
         }
     }
 }
+
+// guards
+pub fn should_abort(sandbox: &str) -> bool {
+    let yes = match std::env::var("CIRCLECI") {
+        Ok(_) => false,
+        Err(_) => match std::env::var("TC_FORCE_DEPLOY") {
+            Ok(_) => false,
+            Err(_) => true,
+        },
+    };
+    yes && (sandbox == "stable")
+}
+
+pub fn guard(sandbox: &str) {
+    if should_abort(sandbox) {
+        std::panic::set_hook(Box::new(|_| {
+            println!("Cannot create stable sandbox outside CI");
+        }));
+        panic!("Cannot create stable sandbox outside CI")
+    }
+}
