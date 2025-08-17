edde55ed7171d2492a8f2c74a7aaac0799fd4481
icylisper <icylisper@gmail.com>
2025-08-17 08:54:27 -0700

    Wip - tests jpath

---
+++ examples/functions/test/handler.py
@@ -0,0 +1,3 @@
+
+def handler(event, context):
+  return {"status": "ok", "message": {"a": 1}}
--- lib/tester/src/lib.rs
+++ lib/tester/src/lib.rs
@@ -4,12 +4,21 @@ use composer::spec::TestSpec;
 use invoker::aws::lambda;
 use std::collections::HashMap;
 use kit as u;
+use serde_json::Value;
+use jsonpath_rust::query::QueryRef;
+use jsonpath_rust::JsonPath;
 use assert_json_diff::{assert_json_eq, assert_json_include};
 
 fn render(s: &str, sandbox: &str) -> String {
     let mut table: HashMap<&str, &str> = HashMap::new();
     table.insert("sandbox", sandbox);
     u::stencil(s, table)
+}
+
+fn query_path(data: Value, path: &str) -> String {
+    println!("query path");
+    let vec: Vec<QueryRef<Value>> = data.query_with_path(path).unwrap();
+    println!("-> {:?}", &vec);
 }
 
 fn assert_case(expected: Option<String>, response: &str, cond: Option<String>) {
@@ -20,7 +29,7 @@ fn assert_case(expected: Option<String>, response: &str, cond: Option<String>) {
     match cond.as_ref() {
         "matches" => assert_json_eq!(response, expected),
         "includes" | "contains" => assert_json_include!(actual: response, expected: expected),
-        _ => println!("JSON path filters not implemented yet")
+        _ => query_path(response, &cond)
     }
 }
 
@@ -47,7 +56,7 @@ pub async fn test(auth: &Auth, sandbox: &str, function: &Function, unit: Option<
             run_unit(auth, &fqn, &t).await;
         }
     } else {
-        println!("Running all test units");
+        println!("Running all test units {}", &tspecs.len());
         for (name, tspec) in tspecs {
             println!("Testing unit {}", &name);
             let fqn = render(&function.fqn, sandbox);
