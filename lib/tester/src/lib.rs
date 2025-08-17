c1c090f982a3eaf41e50aff487bdfc1c7d0e5795
icylisper <icylisper@gmail.com>
2025-08-17 09:50:15 -0700

    Assert json path queries in tester

--- lib/tester/src/lib.rs
+++ lib/tester/src/lib.rs
@@ -3,11 +3,12 @@ use composer::{Function};
 use composer::spec::TestSpec;
 use invoker::aws::lambda;
 use std::collections::HashMap;
+use colored::Colorize;
 use kit as u;
 use serde_json::Value;
-use jsonpath_rust::query::QueryRef;
 use jsonpath_rust::JsonPath;
 use assert_json_diff::{assert_json_eq, assert_json_include};
+use std::time::Instant;
 
 fn render(s: &str, sandbox: &str) -> String {
     let mut table: HashMap<&str, &str> = HashMap::new();
@@ -15,10 +16,12 @@ fn render(s: &str, sandbox: &str) -> String {
     u::stencil(s, table)
 }
 
-fn query_path(data: Value, path: &str) -> String {
-    println!("query path");
-    let vec: Vec<QueryRef<Value>> = data.query_with_path(path).unwrap();
-    println!("-> {:?}", &vec);
+fn assert_json_path(data: Value, expected: Value, path: &str) {
+    let vec: Vec<&Value> = data.query(path).unwrap();
+    assert_json_eq!(
+        vec.iter().collect::<Vec<_>>(),
+        vec![&expected]
+    );
 }
 
 fn assert_case(expected: Option<String>, response: &str, cond: Option<String>) {
@@ -27,14 +30,16 @@ fn assert_case(expected: Option<String>, response: &str, cond: Option<String>) {
     let response = u::json_value(&response);
     let cond = u::maybe_string(cond, "matches");
     match cond.as_ref() {
-        "matches" => assert_json_eq!(response, expected),
+        "matches" | "=" => assert_json_eq!(response, expected),
         "includes" | "contains" => assert_json_include!(actual: response, expected: expected),
-        _ => query_path(response, &cond)
+        _ => assert_json_path(response, expected, &cond)
     }
 }
 
-async fn run_unit(auth: &Auth, fqn: &str, t: &TestSpec) {
+async fn run_unit(auth: &Auth, name: &str, fname: &str, fqn: &str, t: &TestSpec) {
     let TestSpec { payload, expect, condition, .. } = t;
+    let start = Instant::now();
+
     let dir = u::pwd();
     let client = lambda::make_client(&auth).await;
     let payload = invoker::read_payload(auth, &dir, payload.clone()).await;
@@ -44,25 +49,29 @@ async fn run_unit(auth: &Auth, fqn: &str, t: &TestSpec) {
         Err(_) => panic!("Failed to execute test")
     };
     assert_case(expect.clone(), &response, condition.clone());
-    println!("Test Passed");
+    let duration = start.elapsed();
+    let _ = println!("Test unit {} (function/{}) ({}) {:#}",
+                     name, fname, "pass".green(), u::time_format(duration));
 }
 
 pub async fn test(auth: &Auth, sandbox: &str, function: &Function, unit: Option<String>) {
     let tspecs = &function.test;
     if let Some(u) = unit {
         if let Some(t) =  tspecs.get(&u) {
-            println!("Testing unit {}", &u);
             let fqn = render(&function.fqn, sandbox);
-            run_unit(auth, &fqn, &t).await;
+            run_unit(auth, &u, &function.name, &fqn, &t).await;
         }
     } else {
-        println!("Running all test units {}", &tspecs.len());
+        println!("Running all {} test units", &tspecs.len());
         for (name, tspec) in tspecs {
-            println!("Testing unit {}", &name);
             let fqn = render(&function.fqn, sandbox);
-            run_unit(auth, &fqn, &tspec).await;
+            run_unit(auth, &name, &function.name, &fqn, &tspec).await;
         }
     }
+}
+
+pub fn interactive() {
+
 }
 
 pub async fn test_functions(
