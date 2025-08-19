e931a855ae67e62b6f3e07ade1a7404449be5d0e
icylisper <icylisper@gmail.com>
2025-08-18 20:25:03 -0700

    Adds route test and invoker

---
+++ lib/invoker/src/aws/gateway.rs
@@ -0,0 +1,36 @@
+use authorizer::Auth;
+use aws_sdk_apigatewayv2::{
+    Client
+};
+
+pub async fn make_client(auth: &Auth) -> Client {
+    let shared_config = &auth.aws_config;
+    Client::new(shared_config)
+}
+
+
+pub async fn find_api_id(client: &Client, name: &str) -> Option<String> {
+    let r = client
+        .get_apis()
+        .max_results(String::from("1000"))
+        .send()
+        .await
+        .unwrap();
+    let items = r.items;
+    match items {
+        Some(apis) => {
+            for api in apis.to_vec() {
+                match api.name {
+                    Some(n) => {
+                        if n == name {
+                            return api.api_id;
+                        }
+                    }
+                    None => (),
+                }
+            }
+            return None;
+        }
+        None => None,
+    }
+}
--- lib/invoker/src/aws/mod.rs
+++ lib/invoker/src/aws/mod.rs
@@ -2,3 +2,4 @@ pub mod eventbridge;
 pub mod lambda;
 pub mod sfn;
 pub mod s3;
+pub mod gateway;
