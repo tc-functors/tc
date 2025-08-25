8941a9c21e092e79071eb9f6600952eeb50267e7
icylisper <icylisper@gmail.com>
2025-08-24 19:49:40 -0700

    Fix role create for base roles

--- lib/deployer/src/role.rs
+++ lib/deployer/src/role.rs
@@ -35,18 +35,33 @@ async fn create_aux(profile: String, role_arn: Option<String>, role: composer::R
     let auth = Auth::new(Some(profile), role_arn).await;
     let client = iam::make_client(&auth).await;
 
-    if &role.kind.to_str() != "provided"  {
-        let r = Role {
-            client: client.clone(),
-            name: role.name.clone(),
-            trust_policy: role.trust.to_string(),
-            policy_arn: role.policy_arn.clone(),
-            policy_name: role.policy_name.clone(),
-            policy_doc: role.policy.to_string(),
-            tags: Some(iam::make_tags(tags.clone()))
-        };
-        let _ = r.create_or_update().await;
-    }
+    let r = Role {
+        client: client.clone(),
+        name: role.name.clone(),
+        trust_policy: role.trust.to_string(),
+        policy_arn: role.policy_arn.clone(),
+        policy_name: role.policy_name.clone(),
+        policy_doc: role.policy.to_string(),
+        tags: Some(iam::make_tags(tags.clone()))
+    };
+
+    let _ = match role.kind.to_str().as_ref() {
+        "provided" => (),
+        "base" => match std::env::var("TC_UPDATE_BASE_ROLES") {
+            Ok(_) => {
+                let _ = r.create_or_update().await;
+                ()
+            }
+            Err(_) => {
+                r.find_or_create().await;
+                ()
+            }
+        },
+        _ => {
+            let _ = r.create_or_update().await;
+            ()
+        }
+    };
 }
 
 pub async fn create_or_update(auth: &Auth, roles: &HashMap<String, composer::Role>, tags: &HashMap<String, String>) {
