63b998c0644792915d361f9b9ae633a580dd5002
icylisper <icylisper@gmail.com>
2025-09-01 16:02:43 -0700

    Use named containers in inline and code builds

--- lib/builder/src/code/mod.rs
+++ lib/builder/src/code/mod.rs
@@ -65,6 +65,13 @@ async fn build_with_docker(
     let secret_file = format!("/tmp/{}-secret.txt", name);
     let session_file = format!("/tmp/{}-session.txt", name);
 
+
+    let container_sha = format!("{}_{}", name, u::checksum_str(dir));
+
+    let create_cont_str = format!("docker buildx create --platform linux/amd64 --name {container_sha} --use --bootstrap");
+
+    u::sh(&create_cont_str, dir);
+
     let root = &u::root();
     let (key, secret, token) = auth.get_keys().await;
 
@@ -73,7 +80,7 @@ async fn build_with_docker(
     u::write_str(&secret_file, &secret);
     u::write_str(&session_file, &token);
 
-    let cmd_str = format!("docker buildx build --platform=linux/amd64 --provenance=false -t {} --secret id=aws-key,src={} --secret id=aws-secret,src={} --secret id=aws-session,src={}  --build-context shared={root} .",
+    let cmd_str = format!("docker buildx build --platform=linux/amd64 --provenance=false --load -t {} --secret id=aws-key,src={} --secret id=aws-secret,src={} --secret id=aws-session,src={} --builder {container_sha} --build-context shared={root} .",
                           name, &key_file, &secret_file, &session_file);
 
     tracing::debug!("Building with docker {}", &cmd_str);
@@ -128,6 +135,7 @@ pub async fn build(
 
             if !pre.is_empty() {
                 gen_dockerfile(dir, langr, pre);
+
                 let (status, out, err) = build_with_docker(&auth, name, dir).await;
                 sh("rm -f Dockerfile wrapper", dir);
                 if status {
