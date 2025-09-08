5e0f0f3c98b5a51cf8725285ddd3e1742d07796d
icylisper <icylisper@gmail.com>
2025-09-08 12:32:57 -0700

    Snapshotter config test

--- lib/snapshotter/src/pipeline.rs
+++ lib/snapshotter/src/pipeline.rs
@@ -15,6 +15,39 @@ pub fn generate_config(records: &Vec<Manifest>) -> String {
     format!(r#"
 version: 2.1
 
+parameters:
+  tc-deploy-snapshot-pipeline:
+    type: boolean
+    default: false
+  api_call:
+    type: boolean
+    default: false
+
+jobs:
+  hello1:
+    docker:
+      - image: cimg/base:2025.08
+    steps:
+      - run: echo "hello world1"
+
+  hello2:
+    docker:
+      - image: cimg/base:2025.08
+    steps:
+      - run: echo "hello world2"
+
+  hello3:
+    docker:
+      - image: cimg/base:2025.08
+    steps:
+      - run: echo "hello world3"
+
+workflows:
+  all-hellos:
+    jobs:
+      - hello1
+      - hello2
+      - hello3
 
 "#)
 }
