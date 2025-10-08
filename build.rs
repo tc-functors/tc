f6804bf2025521b19d1f61b5ef9aa4269ce1740a
icylisper <icylisper@gmail.com>
2025-10-08 13:09:38 -0700

    Use git tag in build.rs

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use std::process::Command;
 
 fn main() {
     let project_version = "PROJECT_VERSION";
-    let output = Command::new("git").args(&["rev-parse", "--short", "HEAD"]).output().unwrap();
+    let output = Command::new("git").args(&["tag", "--points-at", "HEAD"]).output().unwrap();
     let git_tag = String::from_utf8(output.stdout).unwrap();
     println!("cargo:rustc-env={project_version}={git_tag}");
 }
