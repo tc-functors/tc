1019cb7272f6df350c57745c57e6fce4bdaa0fb3
icylisper <icylisper@gmail.com>
2025-10-09 17:11:14 -0700

    Restructure examples

--- examples/apps/api-async/processor/handler.py
+++ examples/apps/api-async/processor/handler.py
@@ -1,3 +1,24 @@
+import json
+import time
+
+# TODO: for some reason detail is not JSON. Yet to figure out the transformation. This is a hack until then
+def parse_detail(detail):
+    detail = detail.strip('{').strip('}')
+    return dict(i.split("=") for i in detail.split(", "))
 
 def handler(event, context):
-  return {"status": "ok"}
+  print('received event:')
+  print(event)
+  args = event.get('arguments')
+  detail = parse_detail(args.get('detail'))
+  id = detail.get('id')
+
+  time.sleep(2)
+
+  response = {
+    'id': id,
+    'message': "Processed Message "
+  }
+
+  print(response)
+  return response
