16221ccef781f4a1b47b3efc36d60aee98a03c6d
icylisper <icylisper@gmail.com>
2025-11-05 19:23:49 -0800

    Adds target and source entity in sequence

--- lib/composer/src/sequence.rs
+++ lib/composer/src/sequence.rs
@@ -3,8 +3,12 @@ use serde_derive::{
     Serialize,
 };
 
+use itertools::Itertools;
+
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Connector {
+    pub source_entity: String,
+    pub target_entity: String,
     pub source: String,
     pub message: String,
     pub target: String
@@ -15,13 +19,17 @@ pub fn make_seq(cspecs: &Vec<String>) -> Vec<Connector> {
         for x in cspecs {
             let s = x.replace(" ", "");
             let parts: Vec<&str> = s.split("->").collect();
-            let source = parts.clone().into_iter().nth(0).unwrap_or_default();
+            let source_raw = parts.clone().into_iter().nth(0).unwrap_or_default();
+            let (source, source_entity) = source_raw.split("/").collect_tuple().unwrap_or((source_raw, ""));
             let message = parts.clone().into_iter().nth(1).unwrap_or_default();
-            let target = parts.clone().into_iter().nth(2).unwrap_or_default();
+            let target_raw = parts.clone().into_iter().nth(2).unwrap_or_default();
+            let (target, target_entity) = target_raw.split("/").collect_tuple().unwrap_or((target_raw, ""));
             let c = Connector {
                 source: source.to_string(),
+                source_entity: source_entity.to_string(),
                 message: message.to_string(),
-                target: target.to_string()
+                target: target.to_string(),
+                target_entity: target_entity.to_string(),
             };
             cs.push(c);
         }
