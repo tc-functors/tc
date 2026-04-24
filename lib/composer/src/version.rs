42e5b317dee66b88bbfabf321118fb9475840d61
icylisper <icylisper@gmail.com>
2026-04-23 20:14:13 -0700

    Handle semver detection in compose

--- lib/composer/src/version.rs
+++ lib/composer/src/version.rs
@@ -15,11 +15,11 @@ fn extract_version(s: &str) -> String {
 
 pub fn current_semver(prefix: &str) -> String {
     let cmd = format!(
-        "git describe --match {}-* --tags $(git log -n1 --pretty='%h')",
+        "git describe --match {}-[0-9]*.[0-9]*.[0-9]* --tags $(git log -n1 --pretty='%h')",
         prefix
     );
     let out = sh(&cmd, &pwd());
-    if out.contains("fatal") {
+     if out.contains("fatal") {
         String::from("0.0.1")
     } else {
         extract_version(&out)
