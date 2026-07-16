e4631485d3ed4035c0df1be1de9a6bc2271db801
icylisper <icy@lisper.cc>
2026-07-15 22:35:21 -0700

    Bump timeout in constants

--- lib/provider/src/aws/constants.rs
+++ lib/provider/src/aws/constants.rs
@@ -6,13 +6,13 @@ use std::time::Duration;
 
 pub const MAX_ATTEMPTS: u32 = 20;
 
-pub const INITIAL_BACKOFF: Duration = Duration::from_secs(1);
+pub const INITIAL_BACKOFF: Duration = Duration::from_secs(3);
 
 pub const MAX_BACKOFF: Duration = Duration::from_secs(10);
 
-const OPERATION_TIMEOUT: Duration = Duration::from_secs(60);
+const OPERATION_TIMEOUT: Duration = Duration::from_secs(300);
 
-const OPERATION_ATTEMPT_TIMEOUT: Duration = Duration::from_millis(10000);
+const OPERATION_ATTEMPT_TIMEOUT: Duration = Duration::from_millis(80000);
 
 pub fn timeout_config() -> TimeoutConfig {
     TimeoutConfig::builder()
