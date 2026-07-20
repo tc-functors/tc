79a2663c4ac936a83e62676ec3c0549be657dd78
icylisper <icy@lisper.cc>
2026-07-19 21:19:29 -0700

    Change Rankdir in digraph format

--- lib/composer/src/formatter/digraph.rs
+++ lib/composer/src/formatter/digraph.rs
@@ -2,12 +2,14 @@ use crate::{
     Topology,
     graph,
 };
+
 use petgraph::dot::{
     Config,
+    RankDir,
     Dot,
 };
 
 pub fn pprint(topology: &Topology) {
     let graph = graph::build_digraph(topology);
-    println!("{:?}", Dot::with_config(&graph, &[Config::EdgeNoLabel]));
+    println!("{:?}", Dot::with_config(&graph, &[Config::EdgeNoLabel, Config::RankDir(RankDir::LR)]));
 }
